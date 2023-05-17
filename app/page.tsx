6fd4949d189425a2f27395a07bd675269fb30d74
agentMAK <martin0044@gmail.com>
2023-05-17 15:27:22 +0100

    Read from a contract

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,18 @@
 import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import { ethers } from "ethers";
 import { useState } from "react";
+import erc20abi from '../contracts/abis/erc20.abi.json'
 
 export default function Home() {
+
+  const usdcAddress = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+  const testAddress = '0x8e4fD0a080818377ADEf093D99392813c3526298'
+
   const [currentAccount, setCurrentAccount] = useState<string | undefined>(
+    undefined
+  );
+
+  const [balance, setBalance] = useState<string | undefined>(
     undefined
   );
 
@@ -39,6 +48,13 @@ export default function Home() {
     }
   };
 
+  const readContractBalance = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const erc20USDC = new ethers.Contract(usdcAddress, erc20abi, provider);
+    const balance = await erc20USDC.balanceOf(testAddress);
+    setBalance(balance.toString())
+  }
+
 
   return (
     <Box mt={"50px"} textAlign={"center"}>
@@ -49,6 +65,10 @@ export default function Home() {
         Connect Wallet
       </Button>
       <Text>Wallet Address: {currentAccount}</Text>
+      <Button mt={'40px'} mb={"5px"} onClick={readContractBalance}>
+        Get USDC balance of test user (Ethereum)
+      </Button>
+      <Text>Balance: {balance}</Text>
     </Box>
   );
 }
